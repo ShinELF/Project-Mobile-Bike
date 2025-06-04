82ff6fb8c09e2218cf455e8e5cfa836b7af86775
Alice Tiertant <alice.tiertant@gmail.com>
2025-06-04 14:03:40 +0200

    création page boutique

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -68,4 +68,20 @@ class HomeController extends Controller
       $html
     );
   } //View::baseTemplate("Accueil", $html)
+        public function shop(ServerRequestInterface $request): ResponseInterface
+  {
+
+    $html = $this->twig->render('home/boutique.twig', [
+      'title' => "Boutique en ligne",
+      'links' => $this->navService->routesToLinks('/boutique'),
+      'date' => date(DATE_ATOM, strtotime('now'))
+    ]);
+    /* $html = View::header($links);
+    $html .= "<h1>Page d'accueil</h1>"; */
+    return new Response(
+      200,
+      ['Content-Type' => 'text/html'],
+      $html
+    );
+  } //View::baseTemplate("Accueil", $html)
 }
