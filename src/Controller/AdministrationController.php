5a684cb0f4eb78723893b2893ff30c15076abe42
Alice Tiertant <alice.tiertant@gmail.com>
2025-06-30 11:38:26 +0200

    add product repository to admincontroller

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -15,33 +15,35 @@ class AdministrationController extends Controller
   // Il faut créer un constructeur qui va récupérer les routes (dans les paramètres)
   // On stocke les routes dans un attribut $routes
   // on crée une méthode qui permet de créer le tableau links à partir de $routes
-    // Attributs
+  // Attributs
   private $productRepository;
-  
+
   public function __construct(NavigationService $navService, Environment $twig)
   {
+    $this->productRepository = new ProductRepository();
     $this->navService = $navService;
     $this->twig = $twig;
   }
-  
 
-    public function pageAdministration(ServerRequestInterface $request): ResponseInterface
-    {
 
-        $html = $this->twig->render('home/administration.twig', [
-            'title' => "Administration",
-            'links' => $this->navService->routesToLinks('/administration'),
-            'date' => date(DATE_ATOM, strtotime('now'))
-        ]);
-        /* $html = View::header($links);
+  public function pageAdministration(ServerRequestInterface $request): ResponseInterface
+  {
+    $product = $this->productRepository->findAll();
+    $html = $this->twig->render('home/administration.twig', [
+      'title' => "Administration",
+      'links' => $this->navService->routesToLinks('/administration'),
+      'date' => date(DATE_ATOM, strtotime('now')),
+      'product' => $product
+    ]);
+    /* $html = View::header($links);
         $html .= "<h1>Page d'accueil</h1>"; */
-        return new Response(
-            200,
-            ['Content-Type' => 'text/html'],
-            $html
-        );
-    }
-      public function event(ServerRequestInterface $request): ResponseInterface
+    return new Response(
+      200,
+      ['Content-Type' => 'text/html'],
+      $html
+    );
+  }
+  public function event(ServerRequestInterface $request): ResponseInterface
   {
 
     $html = $this->twig->render('home/evenement.twig', [
@@ -56,43 +58,42 @@ class AdministrationController extends Controller
     );
   }
 
-    public function findAll(ServerRequestInterface $request): ResponseInterface
-    {
+  public function findAll(ServerRequestInterface $request): ResponseInterface
+  {
 
-        $title = "Boutique en ligne";
-        // Récupération des produit de type Model\Product car c'est le repository (ici AbstractRepository) qui fait la 
-        // correspondance (mapping) entre la base de données et le "Model" objet.
-        $product = $this->productRepository->findAll();
+    $title = "Boutique en ligne";
+    // Récupération des produit de type Model\Product car c'est le repository (ici AbstractRepository) qui fait la 
+    // correspondance (mapping) entre la base de données et le "Model" objet.
 
-        $html = $this->twig->render('shop/index.twig', [
-            'title' => $title,
-            'links' => $this->navService->routesToLinks('/boutique'),
-            'product' => $product
-        ]);
 
-        return new Response(
-            200,
-            ['Content-Type' => 'text/html'],
-            $html
-        );
-    }
-    public function pageInscription(ServerRequestInterface $request): ResponseInterface
-    {
+    $html = $this->twig->render('shop/index.twig', [
+      'title' => $title,
+      'links' => $this->navService->routesToLinks('/boutique'),
 
-        $html = $this->twig->render('home/inscription.twig', [
-            'title' => "Inscription",
-            'links' => $this->navService->routesToLinks('/inscription'),
-        ]);
-        /* $html = View::header($links);
+    ]);
+
+    return new Response(
+      200,
+      ['Content-Type' => 'text/html'],
+      $html
+    );
+  }
+  public function pageInscription(ServerRequestInterface $request): ResponseInterface
+  {
+
+    $html = $this->twig->render('home/inscription.twig', [
+      'title' => "Inscription",
+      'links' => $this->navService->routesToLinks('/inscription'),
+    ]);
+    /* $html = View::header($links);
         $html .= "<h1>Page d'accueil</h1>"; */
-        return new Response(
-            200,
-            ['Content-Type' => 'text/html'],
-            $html
-        );
-
-    }
-      public function subsciptionSuccess(ServerRequestInterface $request): ResponseInterface
+    return new Response(
+      200,
+      ['Content-Type' => 'text/html'],
+      $html
+    );
+  }
+  public function subsciptionSuccess(ServerRequestInterface $request): ResponseInterface
   {
 
     $title =  "Ajout d'un utilisateur";
